ce81a8635b6662c0e4533d168825d28741898ed3
Tathagata Talukdar <tathagata.talukdar@gmail.com>
2024-04-15 22:00:59 +0530

    database create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='*' element={<Home/>}/>
       </Routes>
       </div>
     </Router>
