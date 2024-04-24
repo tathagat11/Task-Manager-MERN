b2f0be4864b2fd0709283a32f4849047a962c76f
Tathagata Talukdar <tathagata.talukdar@gmail.com>
2024-04-24 12:31:21 +0530

    Tasks added to table

--- client/src/components/taskmanager/ListCard.jsx
+++ client/src/components/taskmanager/ListCard.jsx
@@ -5,6 +5,12 @@ const ListCard = ({item}) => {
   console.log(item);
   return (
     <div>
+      <ul className="menu">
+        <li className="menuitems">{item._id}</li>
+        <li className="menuitems">{item.task}</li>
+        <li className="menuitems">{item.status}</li>
+        <li className="menuitems"><button>Do</button></li>
+      </ul>
     </div>
   )
 }
